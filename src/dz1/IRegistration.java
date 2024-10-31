24aab0fbf1e826828462198c048a9093dc72fcd6
oINCLUDEo <nikita-zhuklin@rambler.ru>
2024-10-31 21:59:25 +0400

    Homework 2 Done && Project restructure

--- src/dz1/IRegistration.java
+++ src/dz1/IRegistration.java
@@ -1,3 +1,5 @@
+package dz1;
+
 public interface IRegistration {
 
     void registrate();
--- src/dz1/Main.java
+++ src/dz1/Main.java
@@ -1,3 +1,5 @@
+package dz1;
+
 public class Main {
     public static void main(String[] args){
         Car car = new Car();
@@ -7,6 +9,6 @@ public class Main {
         Airplane airplane = new Airplane(160, "1998-05-22", "2022-10-07");
         airplane.getAirplane_info();
 
-        System.out.println("Примерная скорость разгона до 100 км/ч - " + Car.acceleration_time(249, 3456));
+        System.out.println("Р Р°Р·РіРѕРЅ РґРѕ 100 РєРј/С‡ - " + Car.acceleration_time(249, 3456));
     }
 }
