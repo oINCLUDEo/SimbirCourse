da7c0bacdf8b43c41983fd5a1370d5b8bf2fba59
oINCLUDEo <nikita-zhuklin@rambler.ru>
2024-12-09 20:29:16 +0400

    Lesson 5 Fixed

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -41,9 +41,7 @@ public class SelenideTest {
     }
 
     @BeforeMethod(onlyForGroups = "edge")
-    public void setUpEdgeConfig() {
-        Configuration.browser = "edge";
-    }
+    public void setUpEdgeConfig() { Configuration.browser = "edge"; }
 
     @BeforeMethod(onlyForGroups = "chrome")
     public void setUpChromeConfig() { Configuration.browser = "chrome"; }
@@ -56,6 +54,7 @@ public class SelenideTest {
         $(By.id("all-main-news-link")).click();
         $(By.id("bx_breadcrumb_1")).shouldBe(visible);
     }
+
     @Test(groups = {"non-headless", "edge"})
     public void ulstuCalendarTest(){
         logger.info("Тест Календаря на сайте УлГТУ");
@@ -66,7 +65,7 @@ public class SelenideTest {
 
         $(By.xpath("//*[@id=\"comp_92505226034e58f28aba9a8ff60ad5c8\"]/div/div[1]/div[2]/ul[4]/li[6]/a")).click();
         $(By.xpath("//*[@id=\"comp_92505226034e58f28aba9a8ff60ad5c8\"]/div/div[2]/div[1]/p/b")).shouldHave(text("21 декабря 2024 года"));
-        sleep(5000);
+        sleep(1000);
     }
 
     public static void scrollToElement(SelenideElement element) {
@@ -82,8 +81,8 @@ public class SelenideTest {
         open("https://store.steampowered.com/?l=russian");
         $(By.xpath("//*[@id=\"global_action_menu\"]/a[2]")).shouldBe(visible).click();
         $(By.id("loginModals")).shouldBe(visible);
-        sleep(2500);
     }
+
     @Test(groups = {"non-headless", "chrome"})
     public void steamTest2(){
         logger.info("Запущен тест ссылки 'О компании' на сайте Steam");
